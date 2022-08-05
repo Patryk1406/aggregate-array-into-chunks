ad3cd94fbf1d3993689c5264a8f604e5d996d019
Patryk Kuropatwiński <patryk.kuropatwinski@signalos.io>
2022-08-05 16:33:20 +0200

    v2

--- aggregate-array-into-chunks.ts
+++ aggregate-array-into-chunks.ts
@@ -4,13 +4,13 @@ const aggregateArrayIntoChunks = <T>(array: T[]): T[][] => {
   if (array.length < 4) throw new Error('The input array cannot have less than 4 elements.');
   const copy = array.slice();
   const returnedArray: T[][] = [];
-  let lengthOfCurrentChunk;
+  // let nextChunkLength;
   while (copy.length > 7) {
-    lengthOfCurrentChunk = generateRandomNumberInRange(4, 8);
-    if (copy.length - lengthOfCurrentChunk < 4) {
-      lengthOfCurrentChunk = generateRandomNumberInRange(4, copy.length - 4 + 1);
+    let nextChunkLength = generateRandomNumberInRange(4, 7);
+    if (copy.length - nextChunkLength < 4) {
+      nextChunkLength = generateRandomNumberInRange(4, copy.length - 4);
     }
-    returnedArray.push(copy.splice(0, lengthOfCurrentChunk));
+    returnedArray.push(copy.splice(0, nextChunkLength));
   }
   returnedArray.push(copy);
   return returnedArray;
